bdf0cd10aa681b5c229e253a4968dba08c5c4c6d
Peter Kang <kangguolong@gmail.com>
2020-09-15 23:54:12 +1200

    Submit westpac rest project architecture. closes #1

---
+++ src/main/java/com/westpac/mockweb/restservice/WestpacRestApplication.java
@@ -0,0 +1,13 @@
+package com.westpac.mockweb.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WestpacRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WestpacRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/westpac/mockweb/restservice/WestpacRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westpac.mockweb.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WestpacRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
