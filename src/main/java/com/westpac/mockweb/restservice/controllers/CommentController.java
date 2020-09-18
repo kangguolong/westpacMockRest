9bafe58941428208f783d462fd8b0658bb6bc210
Peter Kang <kangguolong@gmail.com>
2020-09-18 19:44:14 +1200

    Add Swagger support for APIs. Closes #8

--- src/main/java/com/westpac/mockweb/restservice/controllers/CommentController.java
+++ src/main/java/com/westpac/mockweb/restservice/controllers/CommentController.java
@@ -2,6 +2,7 @@ package com.westpac.mockweb.restservice.controllers;
 
 import com.westpac.mockweb.restservice.api.domain.Comment;
 import com.westpac.mockweb.restservice.services.CommentApiService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -17,6 +18,7 @@ public class CommentController {
         this.commentApiService = commentApiService;
     }
 
+    @Operation(summary = "Get a comment list by a given post id")
     @GetMapping("/byPostId/{postId}")
     @ResponseStatus(HttpStatus.OK)
     public Flux<Comment> getCommentsByPostId(@PathVariable Integer postId) {
